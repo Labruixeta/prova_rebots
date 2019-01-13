a63c14a545434aae359d32d79599291237466c89
La Bruixeta <albert.emporda@gmail.com>
2019-01-14 00:55:40 +0100

    Estat inicial

---
+++ Assets/ControlEsfera.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum EstatEsfera {Neix,Creix,Repro,Mort}
+
+public class ControlEsfera : MonoBehaviour {
+    public int contador = 1;
+    public Vector3 posicio;
+    public GameObject esfera;
+    public EstatEsfera estatesfera = EstatEsfera.Neix;
+
+    void Start ()
+    {
+        esfera = Resources.Load<GameObject>("Sphere");
+        GetComponent<Transform>().localScale = new Vector3(0f, 0f, 0f);
+        //GetComponent<Rigidbody> ().velocity = new Vector2 (Random.Range(-5f,5f),Random.Range(-5f,5f));
+	}
+	
+	void Update ()
+    {
+        //---------------------------------------------------------------------------------
+        //                                      NEIXEMENT
+        //---------------------------------------------------------------------------------
+        if (estatesfera == EstatEsfera.Neix)
+        {
+            contador++;
+            if (contador == 10)
+            {
+                GetComponent<Transform>().localScale = new Vector3(GetComponent<Transform>().localScale.x + 0.1f, GetComponent<Transform>().localScale.y + 0.1f, GetComponent<Transform>().localScale.z + 0.1f);
+
+                if (GetComponent<Transform>().localScale.x > 0.9f)
+                {
+                    estatesfera = EstatEsfera.Creix;
+                    GetComponent<Rigidbody> ().velocity = new Vector2 (Random.Range(-2f,2f),Random.Range(-2f,2f));
+                }
+                contador = 1;
+            }
+        }
+        //---------------------------------------------------------------------------------
+        //                                      CREIXEMENT
+        //---------------------------------------------------------------------------------
+       else if (estatesfera == EstatEsfera.Creix)
+        {
+            contador++;
+            if (contador == 10)
+            {
+                GetComponent<Transform>().localScale = new Vector3(GetComponent<Transform>().localScale.x * 1.005f, GetComponent<Transform>().localScale.y * 1.005f, GetComponent<Transform>().localScale.z * 1.005f);
+                if (GetComponent<Transform>().localScale.x > 1.5f)
+                {
+                    estatesfera = EstatEsfera.Repro;
+                }
+                contador = 1;
+            }
+        }
+        //---------------------------------------------------------------------------------
+        //                                      REPRODUCCIó
+        //---------------------------------------------------------------------------------
+        else if (estatesfera == EstatEsfera.Repro)
+        {
+                posicio = transform.position;
+                posicio = new Vector3(posicio.x + 0.2f, posicio.y + 0.2f, posicio.z);
+                Instantiate(esfera, posicio, Quaternion.identity);
+                GetComponent<Transform>().localScale = new Vector3(1f, 1f, 1f);
+                estatesfera = EstatEsfera.Creix;
+        }
+    }
+}
---
+++ Assets/MenjadorControlScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenjadorControlScript : MonoBehaviour {
+	public bool estatMenjador = false;
+	public GameObject menjar;
+	public Vector3 posicioMenjar;
+
+	void Start () {
+	}
+	
+
+	void Update () {
+		if (estatMenjador == false) {
+			if (GameObject.FindGameObjectWithTag ("Menjar") != null) {
+				menjar = GameObject.FindGameObjectWithTag ("Menjar");
+				estatMenjador = true;
+			} else {
+				print ("Tot Menjat!");
+			}
+		} else {
+			posicioMenjar = menjar.transform.position;
+			GetComponent<Rigidbody>().transform.position = Vector3.MoveTowards(transform.position, posicioMenjar, 10f * Time.deltaTime);
+			estatMenjador = false;
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag=="Menjar"){
+			Destroy (other.gameObject);
+		}
+	}
+
+
+
+
+}
